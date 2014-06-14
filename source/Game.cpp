714fa08bc7b6a21501520a093dd91be5e780d0e2
Justin Mazzola <jmazzola@fullsail.edu>
2014-06-14 15:46:27 -0400

    Added so that it doesn't unpause if the player toggles fullscreen while paused.

--- source/Game.cpp
+++ source/Game.cpp
@@ -173,7 +173,10 @@ int Game::Main(void)
 	{
 		ToggleFullscreen();
 		// Disable the 'Enter' input
-		if (m_pCurrState == GameplayState::GetInstance())
+
+		// If we're in the gameplay state and not paused
+		if (m_pCurrState == GameplayState::GetInstance() && !GameplayState::GetInstance()->IsPaused())
+			// Toggle the pause state
 			GameplayState::GetInstance()->SetPaused(!GameplayState::GetInstance()->IsPaused());
 		return false;
 	}
