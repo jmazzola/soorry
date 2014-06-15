4a9eb438eebafcb37a142d05a86ae677bc91c2c7
Justin Patterson <jpatt94@fullsail.edu>
2014-06-14 20:40:54 -0400

    Fixed right-click outside of map crash.

--- source/Player.cpp
+++ source/Player.cpp
@@ -734,7 +734,8 @@ void Player::Update ( float dt )
 		m_nCurrPlaceable = STRAP;
 #endif
 
-	if ((pInput->IsKeyDown(SGD::Key::MouseRight) == true || trg > 0.1f) && m_pZombieWave->IsBuildMode())	{
+	if ((pInput->IsKeyDown(SGD::Key::MouseRight) == true || trg > 0.1f) && m_pZombieWave->IsBuildMode() && Blockable(pos))	
+	{
 		// Test rect
 		SGD::Rectangle rect;
 		rect.left = pos.x * pWorld->GetTileWidth () + pWorld->GetTileWidth () / 4;
