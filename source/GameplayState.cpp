159e0d437416fe125ef038caddd445d0db47411f
Justin Mazzola <jmazzola@fullsail.edu>
2014-06-19 11:36:04 -0400

    Added new LoadSaveState menu picture Added saving on every wave Added deletion of saves when the player loses OR wins.

--- source/GameplayState.cpp
+++ source/GameplayState.cpp
@@ -270,6 +270,8 @@ Player*	GameplayState::CreatePlayer(string _playerStatsFileName) const
 {
 	m_bPlayLaser = false;
 
+	m_bSelectedReplay = false;
+
 	// Load the stats for the stattracker
 	Game* pGame = Game::GetInstance();
 	m_pStatTracker = StatTracker::GetInstance();
@@ -798,7 +800,10 @@ Player*	GameplayState::CreatePlayer(string _playerStatsFileName) const
 	rzbn->SetShop(m_pShop);
 	rzbn->SetWorldManager(WorldManager::GetInstance());
 	rzbn->SetZombieFactory(zombieFactory);
-	SaveGame();
+
+	// If we havent selected replay, save the game
+	/*if (!m_bSelectedReplay)
+		SaveGame();*/
 
 	m_pStatTracker->Save("resource/data/stats.xml");
 	// Release textures
@@ -1330,11 +1335,19 @@ Player*	GameplayState::CreatePlayer(string _playerStatsFileName) const
 			switch (m_bReplay)
 			{
 			case true:
+			{
+				// Delete the current save
+				string wtf = LoadSaveState::GetInstance()->returnGameSaveName(m_nCurrGameSlot);
+				LoadSaveState::GetInstance()->DeleteSave(wtf);
+
+				m_bSelectedReplay = true;
 				Game::GetInstance()->ChangeState(GameplayState::GetInstance());
 				return true;
 				break;
+			}
 
 			case false:
+				m_bSelectedReplay = false;
 				Game::GetInstance()->ChangeState(MainMenuState::GetInstance());
 				return true;
 				break;
@@ -2042,6 +2055,8 @@ Player*	GameplayState::CreatePlayer(string _playerStatsFileName) const
 	// Render the credits if you have won and faded to them
 	else if (m_bCreditsStarted == true)
 	{
+		string wtf = LoadSaveState::GetInstance()->returnGameSaveName(m_nCurrGameSlot);
+		LoadSaveState::GetInstance()->DeleteSave(wtf);
 		RenderCredits();
 	}
 	// Render the Replay menu if you have lost and faded to them
@@ -2334,6 +2349,7 @@ Player*	GameplayState::CreatePlayer(string _playerStatsFileName) const
 		g->m_fSlowHealth *= g->m_fHealthScaling;
 		g->m_fFastHealth *= g->m_fHealthScaling;
 		g->m_fBeaverHealth *= g->m_fHealthScaling;
+		g->SaveGame();
 	}
 		break;
 	case MessageID::MSG_CREATE_GRENADE:
